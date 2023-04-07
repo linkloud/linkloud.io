c76a951882022ab3f571e61fac2f11da98a583da
a-ryang <chnwu.kim@gmail.com>
2023-04-07 09:36:58 +0900

    feat: styled-components
    * styled-components 기본 설정
    
    * PretendardVariable.woff2 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/GlobalStyle";
+import { LIGHT_THEME } from "./styles";
+
 const App = () => {
-  return <div className="App"></div>;
+  // TODO: hooks
+  const [theme, setTheme] = useState("light");
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+  return (
+    <ThemeProvider theme={theme === "light" ? LIGHT_THEME : "TODO"}>
+      <GlobalStyle />
+      Hello World
+    </ThemeProvider>
+  );
 };
 
 export default App;
