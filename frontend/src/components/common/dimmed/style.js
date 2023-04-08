c5028e93b781360e9b0e2d342960ad81268c8783
a-ryang <chnwu.kim@gmail.com>
2023-04-08 23:00:48 +0900

    feat: dimmed 컴포넌트 #1

---
+++ frontend/src/components/common/dimmed/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const DimmedDefault = styled.div`
+  position: fixed;
+  inset: 0;
+  z-index: 999;
+  cursor: pointer;
+  background-color: rgba(0, 0, 0, 0.1);
+`;
