737b717d3e618b979f2da4ab6a0909fd39e42aaa
FaberJoo <sjdk0805@gmail.com>
2023-04-10 18:28:45 +0900

    fix : 예외 응답에 주석 추가

--- backend/src/main/java/io/linkloud/api/global/common/ErrorResponse.java
+++ backend/src/main/java/io/linkloud/api/global/common/ErrorResponse.java
@@ -5,6 +5,7 @@ import jakarta.validation.ConstraintViolation;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
@@ -16,49 +17,66 @@ public class ErrorResponse {
     private List<FieldError> fieldErrors;
     private List<ConstraintViolationError> violationErrors;
 
+    /**
+    * http 상태 코드와 예외 메시지를 받는 생성자 <br/>
+    * ErrorResponse(400, "임시 에러")
+    **/
     private ErrorResponse(int status, String message) {
         this.status = status;
         this.message = message;
     }
 
+    /**
+     * 바디 필드 예외 및 경로 변수 예외를 받는 생성자 <br/>
+     * List<FieldError> fieldErrors = [ <br/>
+     *  { "field: "name", "rejectedValue : "", "reason" : "이름이 공백이 아니어야 합니다." } <br/>
+     *  { "field: "email", "rejectedValue : "", "reason" : "이메일이 공백이 아니어야 합니다." } <br/>
+     * ]; <br/>
+     *
+     * List<ConstraintViolationError> violationErrors = [ <br/>
+     *  { "propertyPath" : "article-id", "message" : "1 이상이어야 합니다", "invalidValue" : -1 } <br/>
+     * ]; <br/>
+     *
+     * ErrorResponse(fieldErrors, violationErrors) <br/>
+    **/
     private ErrorResponse(final List<FieldError> fieldErrors,
         final List<ConstraintViolationError> violationErrors) {
         this.fieldErrors = fieldErrors;
         this.violationErrors = violationErrors;
     }
 
+    // 바디 필드 예외에 해당하는 에러 응답 생성 메소드
     public static ErrorResponse of(BindingResult bindingResult) {
         return new ErrorResponse(FieldError.of(bindingResult), null);
     }
 
+    // 경로 변수 예외에 해당하는 에러 응답 생성 메소드
     public static ErrorResponse of(Set<ConstraintViolation<?>> violations) {
         return new ErrorResponse(null, ConstraintViolationError.of(violations));
     }
 
+    // ExceptionCode에 해당하는 에러 응답 생성 메소드
     public static ErrorResponse of(ExceptionCode exceptionCode) {
         return new ErrorResponse(exceptionCode.getStatus(), exceptionCode.getMessage());
     }
 
+    // HttpStatus에 해당하는 에러 응답 생성 메소드
     public static ErrorResponse of(HttpStatus httpStatus) {
         return new ErrorResponse(httpStatus.value(), httpStatus.getReasonPhrase());
     }
 
+    // HttpStatus에 커스텀 메시지를 넣는 에러 응답 생성 메소드
     public static ErrorResponse of(HttpStatus httpStatus, String message) {
         return new ErrorResponse(httpStatus.value(), message);
     }
 
     // 요청 바디 필드 유효성 검증 예외
     @Getter
+    @AllArgsConstructor
     public static class FieldError {
         private String field;
         private Object rejectedValue;
         private String reason;
-
-        private FieldError(String field, Object rejectedValue, String reason) {
-            this.field = field;
-            this.rejectedValue = rejectedValue;
-            this.reason = reason;
-        }
 
         public static List<FieldError> of(BindingResult bindingResult) {
             final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
@@ -73,17 +91,11 @@ public class ErrorResponse {
 
     // 경로 변수 유효성 검증 예외
     @Getter
+    @AllArgsConstructor
     public static class ConstraintViolationError {
         private String propertyPath;
         private Object rejectedValue;
         private String reason;
-
-        private ConstraintViolationError(String propertyPath, Object rejectedValue,
-            String reason) {
-            this.propertyPath = propertyPath;
-            this.rejectedValue = rejectedValue;
-            this.reason = reason;
-        }
 
         public static List<ConstraintViolationError> of(
             Set<ConstraintViolation<?>> constraintViolations) {
