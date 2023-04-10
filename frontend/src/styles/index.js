d8c427b2ad691c884bb09d1d825ad35d32c8d3ee
a-ryang <chnwu.kim@gmail.com>
2023-04-10 21:54:12 +0900

    feat: FlexRow, FlexColumn 범용 정렬 div 요소

--- frontend/src/styles/index.js
+++ frontend/src/styles/index.js
@@ -2,3 +2,4 @@ export { COLORS } from "./colors";
 export { FONT_SIZE } from "./font-size";
 export { mediaQuery, media } from "./media";
 export { LIGHT_THEME } from "./theme";
+export { FlexRow, FlexColumn } from "./shared";
