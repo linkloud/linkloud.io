5abef7918c6f469fdb32ca8cbb19c6985e017b0f
FaberJoo <sjdk0805@gmail.com>
2023-04-10 14:58:47 +0900

    feat : landingPage Conroller

---
+++ backend/src/main/java/io/linkloud/api/domain/Controller.java
@@ -0,0 +1,14 @@
+package io.linkloud.api.domain;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Controller {
+    @GetMapping
+    public String landingPage() {
+        return "Hello!";
+    }
+}
