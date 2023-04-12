0cd3429cd89df5a38687d8b14a5c40170d88127a
a-ryang <chnwu.kim@gmail.com>
2023-04-12 22:47:58 +0900

    feat: 태그 컨테이너 컴포넌트 #1
    * refactor: tag 파일 위치 수정

--- frontend/src/components/tag/style.js
+++ frontend/src/components/tag/style.js
@@ -1,12 +1,12 @@
 import styled, { css } from "styled-components";
 
-export const TagDefault = styled.div`
+export const Container = styled.a`
   display: inline-flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
   padding: 0 15px;
-  border-radius: 2px;
+  border-radius: 4px;
   line-height: 20px;
   height: 32px;
   cursor: pointer;
@@ -14,6 +14,8 @@ export const TagDefault = styled.div`
   ${({ theme }) => css`
     border: 1px solid ${theme.COLOR.BORDER.MEDIUM};
     background-color: ${theme.COLOR.WHITE};
+    font-size: ${theme.FONT_SIZE.DESCRIPTION};
+
     &:hover {
       border-color: ${theme.COLOR.PRIMARY.HIHG};
     }
