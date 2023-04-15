f1f1a187c1b490363b6811e14e7e7969d8102984
Min <als904204@naver.com>
2023-04-15 19:23:14 +0900

    fix: 주석 오타 수정

--- backend/src/main/java/io/linkloud/api/global/security/auth/client/dto/OAuth2UserInfo.java
+++ backend/src/main/java/io/linkloud/api/global/security/auth/client/dto/OAuth2UserInfo.java
@@ -4,7 +4,7 @@ import io.linkloud.api.domain.member.model.SocialType;
 
 /**
  * GOOGLE,GITHUB 사용자 정보를 JSON 으로 받아와
- * 자바 객체 클래스(GoogleUser) 로 변환했을 때
+ * 자바 객체 클래스(GoogleUserInfo.class) 로 변환했을 때
  * 이 클래스를 상속함으로써
  * OAuthAttributes 클래스 생성자에 이 클래스를 파라매터로 넣음으로써
  * 각 클래스별로 따로 만들지않고 이 클래스로 묶어서 사용가능
