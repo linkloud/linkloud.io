3068a70d3c1f8205aadd85fef99d8f359061448c
Rope-player <dhqnf11@naver.com>
2023-04-17 01:37:29 +0900

    feat: Article 조회(Read) 작성(Controller, Service)

--- backend/src/main/java/io/linkloud/api/domain/article/api/ArticleController.java
+++ backend/src/main/java/io/linkloud/api/domain/article/api/ArticleController.java
@@ -4,9 +4,12 @@ import io.linkloud.api.domain.article.dto.ArticleRequestDto;
 import io.linkloud.api.domain.article.dto.ArticleResponseDto;
 import io.linkloud.api.domain.article.service.ArticleService;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +19,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class ArticleController {
 
     private final ArticleService articleService;
+
+    /** 아티클 모두 조회 */
+    @GetMapping
+    public ResponseEntity<List<ArticleResponseDto>> getAllArticle() {
+        List<ArticleResponseDto> getAllArticleDto = articleService.getAllArticle();
+
+        return new ResponseEntity<>(getAllArticleDto, HttpStatus.OK);
+    }
+
+    /** 아티클 한 개 조회 */
+    @GetMapping("/{id}")
+    public ResponseEntity<ArticleResponseDto> getOneArticle(@PathVariable @Valid Long id) {
+        ArticleResponseDto getOneArticleDto = articleService.getArticleById(id);
+
+        return new ResponseEntity<>(getOneArticleDto, HttpStatus.OK);
+    }
 
     /** 아티클 작성 */
     @PostMapping
--- backend/src/main/java/io/linkloud/api/domain/article/service/ArticleService.java
+++ backend/src/main/java/io/linkloud/api/domain/article/service/ArticleService.java
@@ -4,6 +4,8 @@ import io.linkloud.api.domain.article.dto.ArticleRequestDto;
 import io.linkloud.api.domain.article.dto.ArticleResponseDto;
 import io.linkloud.api.domain.article.model.Article;
 import io.linkloud.api.domain.article.repository.ArticleRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +15,26 @@ import org.springframework.transaction.annotation.Transactional;
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
+
+    /** 아티클 모두 반환 */
+    @Transactional(readOnly = true)
+    public List<ArticleResponseDto> getAllArticle() {
+        List<ArticleResponseDto> articleDtoList = articleRepository.findAll()
+            .stream()
+            .map(ArticleResponseDto::new)
+            .collect(Collectors.toList());  // stream의 결과를 List로 수집
+
+        return articleDtoList;
+    }
+
+    /** 아티클 한 개 반환 */
+    @Transactional
+    public ArticleResponseDto getArticleById(Long id) {
+        Article foundedArticle = articleRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 아티클이 없습니다."));
+        foundedArticle.articleViewIncrease(foundedArticle.getViews() + 1);  // 조회수 증가
+
+        return new ArticleResponseDto(foundedArticle);
+    }
 
     /** 아티클 생성 */
     @Transactional
