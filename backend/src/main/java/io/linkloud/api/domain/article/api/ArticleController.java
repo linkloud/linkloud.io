4ed2686709df3cd471865eb702f71fcdab5fe027
Rope-player <dhqnf11@naver.com>
2023-04-17 01:38:31 +0900

    fix: @RequestMapping 누락 수정

--- backend/src/main/java/io/linkloud/api/domain/article/api/ArticleController.java
+++ backend/src/main/java/io/linkloud/api/domain/article/api/ArticleController.java
@@ -12,10 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/article")
 public class ArticleController {
 
     private final ArticleService articleService;
