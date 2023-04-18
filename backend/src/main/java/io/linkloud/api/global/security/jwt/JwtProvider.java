0ec4d6b68c76969862d913810820e695814fe520
Min <als904204@naver.com>
2023-04-18 13:38:03 +0900

    feat: JwtProvider 클래스 생성, 빈 등록 #13

---
+++ backend/src/main/java/io/linkloud/api/global/security/jwt/JwtProvider.java
@@ -0,0 +1,15 @@
+package io.linkloud.api.global.security.jwt;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class JwtProvider {
+
+
+
+
+}
