a84c599925c26d91e4fb95c75e2c78b1d01077c1
Min <als904204@naver.com>
2023-04-19 20:46:04 +0900

    feat: JwtFilter 클래스 init #13

---
+++ backend/src/main/java/io/linkloud/api/global/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,21 @@
+package io.linkloud.api.global.security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
