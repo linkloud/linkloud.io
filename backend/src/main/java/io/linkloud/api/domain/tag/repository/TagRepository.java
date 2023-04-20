03aed33012447f6ac2c5f33544dbc5d265c36d38
FaberJoo <sjdk0805@gmail.com>
2023-04-20 19:24:32 +0900

    feat: 이름으로 태그 조회를 위한 쿼리 메소드 추가

--- backend/src/main/java/io/linkloud/api/domain/tag/repository/TagRepository.java
+++ backend/src/main/java/io/linkloud/api/domain/tag/repository/TagRepository.java
@@ -2,11 +2,13 @@ package io.linkloud.api.domain.tag.repository;
 
 import io.linkloud.api.domain.tag.model.Tag;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
     Boolean existsByName(String name);
+    Optional<Tag> findTagByName(String name);
     List<Tag> findTagByNameIsStartingWith(String name);
 }
