b572a84f3556307829b35f467c5a37e9a51447ec
FaberJoo <sjdk0805@gmail.com>
2023-04-20 17:23:14 +0900

    feat: 신규 태그 추가 기능 추가

--- backend/src/main/java/io/linkloud/api/domain/tag/Service/TagService.java
+++ backend/src/main/java/io/linkloud/api/domain/tag/Service/TagService.java
@@ -1,7 +1,9 @@
 package io.linkloud.api.domain.tag.Service;
 
-import io.linkloud.api.domain.tag.repository.ArticleTagRepository;
+import io.linkloud.api.domain.tag.model.Tag;
 import io.linkloud.api.domain.tag.repository.TagRepository;
+import io.linkloud.api.global.exception.ExceptionCode;
+import io.linkloud.api.global.exception.LogicException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,9 +13,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TagService {
     private final TagRepository tagRepository;
-    private final ArticleTagRepository articleTagRepository;
 
-    public void addTag() {}
+    public void addTag(Tag tag) {
+        verifyExistTag(tag.getName());
+        Tag addedTag = tagRepository.save(tag);
+
+        log.info("Tag 생성 완료.");
+    }
 
     public void addArticleTag() {}
 
@@ -21,5 +27,12 @@ public class TagService {
 
     public void fetchTagListBySearch() {}
 
-
+    private void verifyExistTag(String tagName) {
+        boolean verifiedTag = tagRepository.existsByName(tagName);
+        if(verifiedTag) {
+            // 임시용 이기 때문에 409 아무거나
+            // TODO : 예외 코드 추가하거나, 태그 생성 기능을 삭제하면 같이 삭제할 예정.
+            throw new LogicException(ExceptionCode.MEMBER_ALREADY_EXISTS);
+        }
+    }
 }
--- backend/src/main/java/io/linkloud/api/domain/tag/api/TagController.java
+++ backend/src/main/java/io/linkloud/api/domain/tag/api/TagController.java
@@ -1,10 +1,14 @@
 package io.linkloud.api.domain.tag.api;
 
+import io.linkloud.api.domain.tag.Service.TagService;
+import io.linkloud.api.domain.tag.dto.TagDto;
+import io.linkloud.api.domain.tag.mapper.TagMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/tags")
 @RequiredArgsConstructor
 public class TagController {
+    private final TagService tagService;
+    private final TagMapper mapper;
 
     // Tag 임시 생성 api
     @PostMapping
-    public ResponseEntity<?> createTag() {
+    public ResponseEntity<?> createTag(@RequestBody TagDto.Post post) {
+        tagService.addTag(mapper.postDtoToTag(post));
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
