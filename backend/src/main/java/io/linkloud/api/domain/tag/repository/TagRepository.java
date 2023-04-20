242070ec2b017014564c4343a00bd70d182f54ca
FaberJoo <sjdk0805@gmail.com>
2023-04-20 21:59:12 +0900

    feat: Pagenation을 위한 쿼리 메소드 추가

--- backend/src/main/java/io/linkloud/api/domain/tag/repository/TagRepository.java
+++ backend/src/main/java/io/linkloud/api/domain/tag/repository/TagRepository.java
@@ -3,6 +3,8 @@ package io.linkloud.api.domain.tag.repository;
 import io.linkloud.api.domain.tag.model.Tag;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,7 @@ import org.springframework.stereotype.Repository;
 public interface TagRepository extends JpaRepository<Tag, Long> {
     Boolean existsByName(String name);
     Optional<Tag> findTagByName(String name);
+    Page<Tag> findAll(Pageable pageable);
     List<Tag> findTagByNameIsStartingWith(String name);
+
 }
