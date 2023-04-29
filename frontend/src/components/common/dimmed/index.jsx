4ba2219d32bfc40f068ec963c9bf8ef2942905f3
a-ryang <chnwu.kim@gmail.com>
2023-04-29 14:55:03 +0900

    fix: 헤더, 검색 높이 계층 문제

--- frontend/src/components/common/dimmed/index.jsx
+++ frontend/src/components/common/dimmed/index.jsx
@@ -2,7 +2,7 @@ const Dimmed = ({ ...props }) => {
   return (
     <div
       {...props}
-      className="fixed inset-0 z-40 cursor-pointer bg-black opacity-30"
+      className="fixed inset-0 cursor-pointer bg-black opacity-30 z-[999]"
     />
   );
 };
--- frontend/src/components/layouts/Header/index.jsx
+++ frontend/src/components/layouts/Header/index.jsx
@@ -5,7 +5,7 @@ import { LogoLabel } from "@/static/svg";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 bg-white border-b-gray-300 z-40">
+    <header className="sticky top-0 bg-white border-b-gray-300 z-50">
       <div className="mx-auto px-4 flex justify-between items-center h-20 max-w-6xl">
         <h1>
           <span className="hidden">linkloud</span>
