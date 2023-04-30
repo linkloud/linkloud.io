1d7d9d0c8367f5cb322eed0567312ed45c97160d
Rope-player <dhqnf11@naver.com>
2023-04-30 20:09:29 +0900

    feat: Pageable 을 이용한 findAll() 작성

--- backend/src/main/java/io/linkloud/api/domain/article/repository/ArticleRepository.java
+++ backend/src/main/java/io/linkloud/api/domain/article/repository/ArticleRepository.java
@@ -1,10 +1,13 @@
 package io.linkloud.api.domain.article.repository;
 
 import io.linkloud.api.domain.article.model.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    Page<Article> findAll(Pageable pageable);
 
 }
