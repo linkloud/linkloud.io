2a10763491b74693fb4074f41c9ffff14be8cbb2
Rope-player <dhqnf11@naver.com>
2023-05-02 17:54:28 +0900

    refactor: 불필요한 @NotBlack 어노테이션 삭제

--- backend/src/main/java/io/linkloud/api/domain/article/dto/ArticleResponseDto.java
+++ backend/src/main/java/io/linkloud/api/domain/article/dto/ArticleResponseDto.java
@@ -1,7 +1,6 @@
 package io.linkloud.api.domain.article.dto;
 
 import io.linkloud.api.domain.article.model.Article;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,28 +10,20 @@ import lombok.Getter;
 @Getter
 public class ArticleResponseDto {
 
-    @NotBlank(message = "id 값이 없습니다.")
     private Long id;
 
-    @NotBlank(message = "member_id 값이 없습니다.")
     private Long member_id;
 
-    @NotBlank(message = "member_nickname 값이 없습니다.")
     private String member_nickname;
 
-    @NotBlank(message = "title 값이 없습니다.")
     private String title;
 
-    @NotBlank(message = "url 값이 없습니다.")
     private String url;
 
-    @NotBlank(message = "description 값이 없습니다.")
     private String description;
 
-    @NotBlank(message = "views 값이 없습니다.")
     private Integer views;
 
-    @NotBlank(message = "bookmarks 값이 없습니다.")
     private Integer bookmarks;
 
 
