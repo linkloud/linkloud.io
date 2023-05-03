b5c682101399d423eebe9e925eaad3dd1683610a
Min <als904204@naver.com>
2023-05-03 23:42:24 +0900

    feat: Jwt필터 액세스 토큰 검증 후 시큐리티에 등록 #24

---
+++ backend/src/main/java/io/linkloud/api/global/security/auth/jwt/dto/SecurityMember.java
@@ -0,0 +1,26 @@
+package io.linkloud.api.global.security.auth.jwt.dto;
+
+import io.linkloud.api.domain.member.model.SocialType;
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+public class SecurityMember extends User {
+    private Long id;
+
+    private String picture;
+
+    public SecurityMember(
+        Long id,
+        String nickname,
+        Collection<? extends GrantedAuthority> authorities,
+        String picture,
+        SocialType socialType
+
+    ) {
+        super(nickname, socialType.name(), authorities);
+        this.id = id;
+        this.picture = picture;
+    }
+
+}
