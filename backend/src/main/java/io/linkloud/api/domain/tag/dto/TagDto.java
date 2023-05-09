f5e0034a0be2fc5d14f707e6762688bf2f031daf
FaberJoo <sjdk0805@gmail.com>
2023-05-09 21:38:17 +0900

    feat: Tag 응답 객체 추가

--- backend/src/main/java/io/linkloud/api/domain/tag/dto/TagDto.java
+++ backend/src/main/java/io/linkloud/api/domain/tag/dto/TagDto.java
@@ -1,5 +1,6 @@
 package io.linkloud.api.domain.tag.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,15 @@ public class TagDto {
     @NoArgsConstructor
     public static class Post {
         private String name;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response {
+        Long id;
+        String name;
+        Long count;
     }
 
 }
