0b444c5046e9adae2cb6d6396448799b8a40ef68
a-ryang <chnwu.kim@gmail.com>
2023-05-14 22:55:08 +0900

    feat: log util

--- frontend/src/common/utils/index.js
+++ frontend/src/common/utils/index.js
@@ -1,6 +1,11 @@
 export * from "./crypto";
 export * from "./storage";
 
+/** 개발 환경에서만 출력되는 로그 */
+export const log = (message) => {
+  if (import.meta.env.DEV) console.log(message);
+};
+
 /**
  * `delay`마다 한번 함수를 실행하는 새로운 함수를 반환한다.
  * @param {Function} func
