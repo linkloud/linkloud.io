976e7248faff82ba64aa0a1dda159b7d13182289
a-ryang <chnwu.kim@gmail.com>
2023-05-14 16:46:38 +0900

    style: 여러 아이콘 추가

--- frontend/src/static/svg/index.js
+++ frontend/src/static/svg/index.js
@@ -7,3 +7,8 @@ export { ReactComponent as ArrowRightIcon } from "./icon-arrow-right.svg";
 export { ReactComponent as WarningAltIcon } from "./icon-warning-alt.svg";
 export { ReactComponent as CheckMarkIcon } from "./icon-checkmark.svg";
 export { ReactComponent as XMarkIcon } from "./icon-x-mark.svg";
+export { ReactComponent as ShareIcon } from "./icon-share.svg";
+export { ReactComponent as KekbabHorizon } from "./icon-kebab-h.svg";
+export { ReactComponent as BookmarkIcon } from "./icon-bookmark.svg";
+export { ReactComponent as BookmarkFillIcon } from "./icon-bookmark-fill.svg";
+export { ReactComponent as LinkOutIcon } from "./icon-link-out.svg";
