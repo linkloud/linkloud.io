f213be87789ddfa786c8d705fe5c6fabb59898b1
a-ryang <chnwu.kim@gmail.com>
2023-05-16 08:38:20 +0900

    feat: useHeader
    * useHeader 커스텀 훅을 이용해 헤더 컴포넌트로부터 액션, 상태 로직 분리

--- frontend/src/common/components/portal/index.jsx
+++ frontend/src/common/components/portal/index.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom";
 
 const Portal = ({ elementId, children }) => {
-  console.log(elementId);
   const potal = document.getElementById(elementId);
   return potal && ReactDOM.createPortal(children, potal);
 };
