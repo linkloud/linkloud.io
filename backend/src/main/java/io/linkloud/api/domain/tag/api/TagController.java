f9925f66bdeabcb21fa706d18d17b4fc1f9c98e7
FaberJoo <sjdk0805@gmail.com>
2023-05-16 16:11:50 +0900

    refactor: tag 리스트 조회 page 필드 유효성 검사 추가

--- backend/src/main/java/io/linkloud/api/domain/tag/api/TagController.java
+++ backend/src/main/java/io/linkloud/api/domain/tag/api/TagController.java
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,13 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/tags")
 @RequiredArgsConstructor
+@Validated
 public class TagController {
     private final TagService tagService;
 
     // Tag 리스트 임시 조회 api
     @GetMapping
     public ResponseEntity<?> getTags(
-        @RequestParam int page,
+        @Positive @RequestParam int page,
         @RequestParam String sortBy) {
         // sortBy 옵션이 존재하는지 확인.
         Tag.SortBy sortField = tagService.verifySortField(sortBy);
