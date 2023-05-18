9757749844c981617d26b0c1d61148fd3b336ab2
Min <als904204@naver.com>
2023-05-18 10:31:21 +0900

    feat:RefreshToken 저장 메서드 #24

--- backend/src/main/java/io/linkloud/api/domain/member/service/RefreshTokenService.java
+++ backend/src/main/java/io/linkloud/api/domain/member/service/RefreshTokenService.java
@@ -1,13 +1,28 @@
 package io.linkloud.api.domain.member.service;
 
+import io.linkloud.api.domain.member.dto.CreateRefreshTokenRequestDto;
+import io.linkloud.api.domain.member.model.RefreshToken;
 import io.linkloud.api.domain.member.repository.RefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class RefreshTokenService {
 
     private final RefreshTokenRepository refreshTokenRepository;
+
+
+    /**
+     * refreshToken 등록
+     * @param dto
+     */
+    public void createRefreshToken(CreateRefreshTokenRequestDto dto) {
+        RefreshToken refreshToken = new RefreshToken(dto.getMemberId(), dto.getRefreshToken());
+        refreshTokenRepository.save(refreshToken);
+        log.info("refreshToken 토큰 저장완료");
+    }
 
 }
