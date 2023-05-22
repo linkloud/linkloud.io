158d0aeecf2a8ac22ff32d28accea59a157a6d5f
Min <als904204@naver.com>
2023-05-22 17:01:20 +0900

    feat:WebConfig 클래스에 Login Member resolver 등록

--- backend/src/main/java/io/linkloud/api/global/config/WebConfig.java
+++ backend/src/main/java/io/linkloud/api/global/config/WebConfig.java
@@ -1,15 +1,29 @@
 package io.linkloud.api.global.config;
 
+import io.linkloud.api.global.security.auth.jwt.JwtProvider;
+import io.linkloud.api.global.security.resolver.LoginMemberIdArgumentResolver;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@RequiredArgsConstructor
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    private final JwtProvider jwtProvider;
 
     @Bean
     public WebClient webClient() {
         return WebClient.builder().build();
     }
 
+
+    @Override
+    public void addArgumentResolvers( List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberIdArgumentResolver(jwtProvider));
+    }
 }
