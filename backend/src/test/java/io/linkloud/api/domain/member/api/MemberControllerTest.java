0b70ca9837a5aa5127ad270e29011fdf568ee07b
Min <als904204@naver.com>
2023-05-23 15:55:55 +0900

    fix:추가된 LoginMemberId 어노테이션에 따라 테스트코드 변경

--- backend/src/test/java/io/linkloud/api/domain/member/api/MemberControllerTest.java
+++ backend/src/test/java/io/linkloud/api/domain/member/api/MemberControllerTest.java
@@ -1,6 +1,7 @@
 package io.linkloud.api.domain.member.api;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doThrow;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -12,12 +13,11 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import io.linkloud.api.domain.member.dto.MemberLoginResponse;
 import io.linkloud.api.domain.member.dto.MemberNicknameRequestDto;
 import io.linkloud.api.domain.member.model.Member;
@@ -27,22 +27,22 @@ import io.linkloud.api.domain.member.repository.MemberRepository;
 import io.linkloud.api.domain.member.service.MemberService;
 import io.linkloud.api.global.exception.CustomException;
 import io.linkloud.api.global.exception.ExceptionCode.LogicExceptionCode;
-import io.linkloud.api.global.security.auth.jwt.dto.SecurityMember;
-import java.util.List;
+import io.linkloud.api.global.security.auth.jwt.JwtProvider;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 @AutoConfigureRestDocs
 @AutoConfigureMockMvc
@@ -53,103 +53,94 @@ class MemberControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-
     @Autowired
-    private ObjectMapper objectMapper;
-
-    @Mock
-    private MemberService memberService;
+    private JwtProvider jwtProvider;
 
     @Autowired
     private MemberRepository memberRepository;
 
+    @MockBean
+    private MemberService memberService;
+
+    @Autowired
+    Gson gson;
+    Member member;
+    MemberLoginResponse memberLoginResponse;
+    MemberNicknameRequestDto memberNicknameRequest;
+    String accessToken;
     @BeforeEach
-    void beforeEach() {
-        memberRepository.save(Member.builder()
-            .id(1L)
-            .nickname("member1_google")
-            .email("member1@email.com")
-            .socialType(SocialType.google)
-            .role(Role.USER)
-            .picture("picture1")
-            .socialId("socialId1")
-            .refreshToken("refreshToken1")
-            .build());
-
-        memberRepository.save(Member.builder()
-            .id(2L)
-            .nickname("member2_google")
-            .email("member2@email.com")
-            .socialType(SocialType.google)
-            .role(Role.USER)
-            .picture("picture2")
-            .socialId("socialId2")
-            .refreshToken("refreshToken2")
-            .build());
+    public void setUp() {
+        member = Member.builder()
+                .id(1L)
+                .nickname("member1_google")
+                .email("member1@email.com")
+                .socialType(SocialType.google)
+                .role(Role.USER)
+                .picture("picture1")
+                .socialId("socialId1")
+                .refreshToken("refreshToken1")
+                .build();
+        memberRepository.save(member);
+        accessToken = jwtProvider.generateAccessToken(member.getId(), member.getSocialType());
+        memberLoginResponse = new MemberLoginResponse(member);
+        memberNicknameRequest = new MemberNicknameRequestDto("new_nickname");
     }
 
-    @DisplayName("회원 조회 성공")
+    @AfterEach
+    public void cleanUp() {
+        memberRepository.findById(member.getId());
+    }
+
+
+
+    String BASE_URL = "/api/v1/member";
+
+
     @Test
-    public void member_me() throws Exception {
+    public void fineMe_success() throws Exception {
 
-        // given
-        Member savedMember = memberRepository.findById(1L)
-            .orElseThrow(() -> new CustomException(LogicExceptionCode.MEMBER_NOT_FOUND));
+        given(memberService.fetchPrincipal(anyLong())).willReturn(memberLoginResponse);
 
-        SecurityMember securityMember = new SecurityMember(
-            savedMember.getId(),
-            savedMember.getNickname(),
-            List.of(new SimpleGrantedAuthority("ROLE_USER")),
-            savedMember.getPicture(),
-            savedMember.getSocialType()
-        );
 
-        MemberLoginResponse memberLoginResponse = new MemberLoginResponse(savedMember);
-        given(memberService.fetchPrincipal(securityMember)).willReturn(memberLoginResponse);
+        ResultActions actions = mockMvc.perform(
+            get(BASE_URL + "/me")
+                .header("Authorization", "Bearer " + accessToken));
 
-        mockMvc.perform(get("/api/v1/member/me").with(user(securityMember)))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.data.nickname").value(savedMember.getNickname()))
-            .andExpect(jsonPath("$.data.picture").value(savedMember.getPicture()))
-            .andExpect(jsonPath("$.data.role").value(Role.USER.toString()))
+        actions
             .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data.nickname").value(member.getNickname()))
+            .andExpect(jsonPath("$.data.picture").value(member.getPicture()))
+            .andExpect(jsonPath("$.data.role").value(member.getRole().name()))
             .andDo(document("member/me/success",
-                    preprocessRequest(prettyPrint()),
-                    preprocessResponse(prettyPrint()),
-                    responseFields(
-                        fieldWithPath("data.nickname").description("회원의 닉네임."),
-                        fieldWithPath("data.picture").description("회원의 프로필 사진 URI."),
-                        fieldWithPath("data.role").description("회원의 권한")
-                    )
-                )
-            );
-
-
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                responseFields(
+                    fieldWithPath("data.nickname").description("회원의 닉네임."),
+                    fieldWithPath("data.picture").description("회원의 프로필 사진 URI."),
+                    fieldWithPath("data.role").description("회원의 권한")
+                )));
     }
+
+
 
     @DisplayName("회원 조회 실패_없는 회원")
     @Test
     public void member_me_not_found_throws() throws Exception {
-
         // given
         doThrow(new CustomException(LogicExceptionCode.MEMBER_NOT_FOUND))
-            .when(memberService).fetchPrincipal(any(SecurityMember.class));
-        Member member = memberRepository.findById(1L).orElseThrow(() -> new IllegalArgumentException("회원을 찾지 못했습니다"));
+            .when(memberService).fetchPrincipal(any(Long.class));
 
-        SecurityMember securityMember = new SecurityMember(
-            10000L,
-            member.getNickname(),
-            List.of(new SimpleGrantedAuthority("ROLE_USER")),
-            member.getPicture(),
-            member.getSocialType()
-        );
+        ResultActions actions = mockMvc.perform(
+            get(BASE_URL + "/me")
+                .header("Authorization", "Bearer " + accessToken));
 
-        mockMvc.perform(get("/api/v1/member/me").with(user(securityMember)))
+        actions
+            .andDo(print())
             .andExpect(status().isNotFound())
             .andExpect(jsonPath("$.status").value(LogicExceptionCode.MEMBER_NOT_FOUND.getStatus()))
             .andExpect(jsonPath("$.message").value(LogicExceptionCode.MEMBER_NOT_FOUND.getMessage()))
-            .andDo(print())
-            .andDo(document("member/me/fail",
+            .andDo(document("member/me/fail/notfound",
                 preprocessRequest(prettyPrint()),
                 preprocessResponse(prettyPrint()),
                 responseFields(
@@ -165,29 +156,16 @@ class MemberControllerTest {
     @Test
     public void updateNickname_success() throws Exception {
 
-        // given
-        Member member1 = memberRepository.findById(1L).orElseThrow(()-> new CustomException(LogicExceptionCode.MEMBER_NOT_FOUND));
-        String newNickname = "new_nickname";
+        String content = gson.toJson(memberNicknameRequest);
 
 
-        SecurityMember securityMember = new SecurityMember(
-            member1.getId(),
-            member1.getNickname(),
-            List.of(new SimpleGrantedAuthority("ROLE_USER")),
-            member1.getPicture(),
-            member1.getSocialType()
-        );
+        ResultActions actions = mockMvc.perform(patch(BASE_URL + "/nickname")
+            .header("Authorization", "Bearer " + accessToken)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(content));
 
-        MemberNicknameRequestDto nicknameRequestDto = new MemberNicknameRequestDto(newNickname);
-
-
-        // when
-        mockMvc.perform(patch("/api/v1/member/nickname")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(nicknameRequestDto))
-                .with(user(securityMember)))
-            .andDo(print())
-            // then
+        actions
             .andExpect(status().isNoContent())
             .andDo(document("member/nickname/success",
                 preprocessRequest(prettyPrint()),
@@ -198,37 +176,25 @@ class MemberControllerTest {
             ));
     }
 
+
     @DisplayName("회원 닉네임 변경 실패 중복 닉네임 예외처리")
     @Test
     public void updateNickname_duplicated_nickname_throws() throws Exception {
-
         // given
-        Member member1 = memberRepository.findById(1L).orElseThrow(()-> new CustomException(LogicExceptionCode.MEMBER_NOT_FOUND));
-        String dupNickname = "member2_google";
-
-        // member1 이 요청
-        SecurityMember securityMember = new SecurityMember(
-            member1.getId(),
-            member1.getNickname(),
-            List.of(new SimpleGrantedAuthority("ROLE_USER")),
-            member1.getPicture(),
-            member1.getSocialType()
-        );
-
-        // member1 이 member2_google 닉네임으로 변경 요청
-        MemberNicknameRequestDto nicknameRequestDto = new MemberNicknameRequestDto(dupNickname);
+        String content = gson.toJson(memberNicknameRequest);
 
         doThrow(new CustomException(LogicExceptionCode.MEMBER_ALREADY_EXISTS))
-            .when(memberService).updateNickname(securityMember, nicknameRequestDto);
+            .when(memberService).updateNickname(anyLong(), any(MemberNicknameRequestDto.class));
 
 
-        // when
-        mockMvc.perform(patch("/api/v1/member/nickname")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(nicknameRequestDto))
-                .with(user(securityMember)))
+        ResultActions actions = mockMvc.perform(patch(BASE_URL + "/nickname")
+            .header("Authorization", "Bearer " + accessToken)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(content));
+
+        actions
             .andDo(print())
-            // then
             .andExpect(status().isConflict())
             .andDo(document("member/nickname/fail/duplicated",
                 preprocessRequest(prettyPrint()),
@@ -237,11 +203,11 @@ class MemberControllerTest {
                     fieldWithPath("nickname").description("수정할 닉네임")
                 ),
                 responseFields(
-                    fieldWithPath("status").description("HTTP 상태 코드. 중복된 닉네임일 경우 409 (Conflict)을 반환합니다."),
+                    fieldWithPath("status").description(
+                        "HTTP 상태 코드. 중복된 닉네임일 경우 409 (Conflict)을 반환."),
                     fieldWithPath("message").description("에러 메시지"),
                     fieldWithPath("fieldErrors").description("필드 에러 리스트"),
                     fieldWithPath("violationErrors").description("벨리데이션 에러 리스트")
                 )));
-
     }
 }
