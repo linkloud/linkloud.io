1cbca08f714c6455da25d65c3df91a44987477a2
FaberJoo <sjdk0805@gmail.com>
2023-05-23 23:37:32 +0900

    feat: WebConfig에 변환자 등록

--- backend/src/main/java/io/linkloud/api/global/config/WebConfig.java
+++ backend/src/main/java/io/linkloud/api/global/config/WebConfig.java
@@ -1,15 +1,23 @@
 package io.linkloud.api.global.config;
 
+import io.linkloud.api.global.converter.StringToTagSortByConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public WebClient webClient() {
         return WebClient.builder().build();
     }
 
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        // config에 converter 등록
+        registry.addConverter(new StringToTagSortByConverter());
+    }
 }
