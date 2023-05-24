2ab88126b0eb117c1e93124452435ce16b578830
a-ryang <chnwu.kim@gmail.com>
2023-05-25 08:35:26 +0900

    refactor: TagListItem 문서구조

--- frontend/src/common/components/tag/TagListItem.jsx
+++ frontend/src/common/components/tag/TagListItem.jsx
@@ -3,14 +3,12 @@ import TagItem from "./TagItem";
 const TagListItem = ({ tag }) => {
   return (
     <>
-      <div className="flex flex-col py-4">
+      <div className="flex flex-col py-4 border-b border-gray-200">
         <div>
           <TagItem tag={tag} />
         </div>
-
         <p className="mt-3.5">링크 {tag.count}개</p>
       </div>
-      <hr></hr>
     </>
   );
 };
