f963bdba72bcfc9688f0032327ee0953ad2e8a06
Min <als904204@naver.com>
2023-05-30 21:38:19 +0900

    feat:로그인,리프레시토큰 요청 시 refreshToken 을 쿠키에 담아 전송 #24

--- backend/src/main/java/io/linkloud/api/domain/member/api/AuthController.java
+++ backend/src/main/java/io/linkloud/api/domain/member/api/AuthController.java
@@ -5,9 +5,13 @@ import io.linkloud.api.domain.member.dto.AuthResponseDto;
 import io.linkloud.api.domain.member.dto.RefreshAccessTokenRequest;
 import io.linkloud.api.global.common.SingleDataResponse;
 import io.linkloud.api.global.security.auth.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +28,8 @@ public class AuthController {
 
     @PostMapping("/{socialType}")
     public ResponseEntity<SingleDataResponse<AuthResponseDto>> authenticate(
-        @RequestBody AuthRequestDto dto) {
-        AuthResponseDto responseDto = authService.authenticate(dto);
+        @RequestBody AuthRequestDto dto, HttpServletResponse response) {
+        AuthResponseDto responseDto = authService.authenticate(dto,response);
         log.info("첫 토큰 생성");
         log.info("AccessToken={}",responseDto.getAccessToken());
         log.info("RefreshToken={}",responseDto.getRefreshToken());
@@ -33,13 +37,12 @@ public class AuthController {
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<SingleDataResponse<AuthResponseDto>> refreshAccessToken(@RequestBody
-    RefreshAccessTokenRequest dto) {
-        AuthResponseDto responseDto = authService.refreshTokenAndAccessToken(dto.getRefreshToken(),
-            dto.getTokenType());
+    public ResponseEntity<SingleDataResponse<AuthResponseDto>> refreshAccessToken(
+        @CookieValue("refreshToken") String refreshToken) {
+        AuthResponseDto responseDto = authService.refreshTokenAndAccessToken(refreshToken);
         log.info("토큰 재생성");
-        log.info("AccessToken={} ",responseDto.getAccessToken());
-        log.info("RefreshToken={} ",responseDto.getRefreshToken());
+        log.info("AccessToken={} ", responseDto.getAccessToken());
+        log.info("RefreshToken={} ", responseDto.getRefreshToken());
         return ResponseEntity.ok(new SingleDataResponse<>(responseDto));
     }
 }
