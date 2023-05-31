bbcc3d995708a3f334ef5e7e5bf9ecc6bbc4b590
Rope-player <dhqnf11@naver.com>
2023-05-31 12:16:55 +0900

    feat: 검색을 위한 JPA 메서드 추가

--- backend/src/main/java/io/linkloud/api/domain/article/repository/ArticleRepository.java
+++ backend/src/main/java/io/linkloud/api/domain/article/repository/ArticleRepository.java
@@ -8,6 +8,19 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    // 조건 없이 모든 아티클 조회
     Page<Article> findAll(Pageable pageable);
+
+    /*
+     * Containing : 검색 기능(LIKE)
+     * IgnoreCase : 대소문자 구분 없음.
+     * */
+
+    // 제시된 키워드를 제목(title)으로 가지는 엔티티 조회
+    Page<Article> findByTitleContainingIgnoreCase(String keyword, Pageable pageable);
+
+    // 제시된 키워드를 내용(description)으로 가지는 엔티티 조회
+    Page<Article> findByDescriptionContainingIgnoreCase(String keyword, Pageable pageable);
 
 }
