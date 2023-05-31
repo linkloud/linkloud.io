143a010eda7198041e3d16fce9d3ccae05eaf240
Min <als904204@naver.com>
2023-05-31 22:44:52 +0900

    refactor:사용하지 않는 예외코드, 잘못된 예외코드 리펙토링

--- backend/src/main/java/io/linkloud/api/global/exception/ExceptionCode/AuthExceptionCode.java
+++ backend/src/main/java/io/linkloud/api/global/exception/ExceptionCode/AuthExceptionCode.java
@@ -5,27 +5,15 @@ import lombok.Getter;
 
 @AllArgsConstructor
 public enum AuthExceptionCode implements ExceptionCode {
-    BAD_REQUEST(400, "Bad Request"),
-    TOKEN_NOT_FOUND(400, "Refresh token not found"),
-    ACCESS_TOKEN_INVALID_01(401, "Invalid Access token"),
 
-    REQUIRED_COOKIE(401, "Required cookie"),
-    REFRESH_TOKEN_INVALID(401, "Invalid Refresh token"),
-    ACCESS_TOKEN_EXPIRED(401, "Access token expired"),
-    REFRESH_TOKEN_EXPIRED(401, "Refresh token expired"),
-    AUTHORIZED_FAIL(403, "Authorized Fail"),
-    USER_UNAUTHORIZED(403, "User unauthorized"),
+    // OAuth 소셜타입 구현체 클래스를 찾지 못할때
+    INVALID_SOCIAL_TYPE(404, "Invalid Social Type requested"),
 
-    // OAuth 액세스토큰 요청 구현체 클래스를 찾지 못할때
-    INVALID_SOCIAL_TYPE(404, "Invalid Social Type requested."),
+    // Redis 에서 refreshToken 을 찾지 못했을 때
+    REFRESH_TOKEN_NOT_FOUND(400, "Refresh token not found"),
 
-    /** JWT Exception Code Start **/
-    INVALID_TOKEN(400, "Jwt token is null or empty or only whitespace."),
-    EXPIRED_TOKEN(401, "Expired JWT token."),
-    UNSUPPORTED_TOKEN(400, "Unsupported JWT token."),
-    MALFORMED_TOKEN(400, "Malformed JWT token.");
-    /** JWT Exception Code End **/
-
+    // 유효하지 않은 토큰일 경우
+    INVALID_TOKEN(401, "Invalid Token");
     @Getter
     private int status;
 
