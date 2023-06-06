8dbf368b5cf1961f587967b9f1b6786dd4813b67
a-ryang <chnwu.kim@gmail.com>
2023-06-07 01:00:33 +0900

    chore: seo

--- frontend/src/common/components/SEO/index.jsx
+++ frontend/src/common/components/SEO/index.jsx
@@ -3,11 +3,18 @@ import { Helmet } from "react-helmet-async";
 const Seo = ({ title, description, type, name }) => {
   return (
     <Helmet>
+      {/* HTML meta tag list */}
       <title>{title}</title>
       <meta name="description" content={description} />
+
+      {/* Facebook meta tag list */}
+      <meta property="og:url" content="https://linkloud.io" />
       <meta property="og:type" content={type} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
+
+      {/* Twitter meta tag list */}
+      <meta property="twitter:domain" content="linkloud.io"></meta>
       <meta name="twitter:creator" content={name} />
       <meta name="twitter:card" content={type} />
       <meta name="twitter:title" content={title} />
