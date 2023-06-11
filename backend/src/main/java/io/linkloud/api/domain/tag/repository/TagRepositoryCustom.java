51f768f4b4003da360bcc47d52c8c6442008adfa
FaberJoo <sjdk0805@gmail.com>
2023-06-11 16:00:00 +0900

    cleanUp: 사용하지 않는 DSL 제거

--- backend/src/main/java/io/linkloud/api/domain/tag/repository/TagRepositoryCustom.java
+++ backend/src/main/java/io/linkloud/api/domain/tag/repository/TagRepositoryCustom.java
@@ -1,15 +1,11 @@
 package io.linkloud.api.domain.tag.repository;
 
 import io.linkloud.api.domain.tag.dto.TagDto;
-import io.linkloud.api.domain.tag.model.Tag;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface TagRepositoryCustom {
-    Boolean existsByName(String name);
-    Optional<Tag> findTagByName(String name);
     Page<TagDto.Response> findAllOrderBy(Pageable pageable);
     List<TagDto.Response> findTagByNameIsStartingWith(String name);
 
