c1a61ba0c45ccf0e8618dd99c2dadd865f532dc6
a-ryang <chnwu.kim@gmail.com>
2023-06-11 13:02:24 +0900

    style: 태그 아이템 UI

--- frontend/src/common/components/tag/TagItem.jsx
+++ frontend/src/common/components/tag/TagItem.jsx
@@ -4,9 +4,9 @@ const TagItem = ({ tag }) => {
   return (
     <Link
       to={`/search?tag=${tag.name}`}
-      className="inline-flex justify-center px-3.5 py-1 rounded text-sm cursor-pointer text-indigo-400 bg-indigo-50 hover:bg-indigo-100 transition-colors"
+      className="inline-flex justify-center px-2 py-1.5 rounded text-xs cursor-pointer text-indigo-400 bg-indigo-50 hover:bg-indigo-100 transition-colors"
     >
-      #{tag.name}
+      {tag.name}
     </Link>
   );
 };
