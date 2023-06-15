ed866f8f6c423c4a11ace887dfd182ab091894e9
a-ryang <chnwu.kim@gmail.com>
2023-06-15 21:49:56 +0900

    test: useArticleSearch

--- frontend/src/hooks/article/__test__/useArticleSearch.test.jsx
+++ frontend/src/hooks/article/__test__/useArticleSearch.test.jsx
@@ -26,46 +26,23 @@ describe("useArticleSearch TEST", () => {
     );
   });
 
-  it("[fail] 검색 키워드가 없다면 예외가 발생한다", () => {
+  it("[success] 태그 없이 검색 경로로 이동한다", () => {
     const { result } = renderHook(() => useArticleSearch());
 
     act(() => {
-      result.current.handleSearch("");
+      result.current.handleSearch("test");
     });
 
-    expect(result.current.searchValidationErrMsg).toEqual(
-      "검색어를 입력해 주세요."
-    );
+    expect(useNavigate()).toHaveBeenCalledWith("/search?keyword=test");
   });
 
-  it("[fail] 태그 검색 최대 5개를 초과할 경우 예외가 발생한다", () => {
+  it("[success] 중복된 태그는 제거 후 검색 경로로 이동한다", () => {
     const { result } = renderHook(() => useArticleSearch());
 
     act(() => {
-      result.current.handleSearch(
-        "test [tag1] [tag2] [tag3] [tag4] [tag5] [tag6]"
-      );
+      result.current.handleSearch("test [tag1] [tag1]");
     });
 
-    expect(result.current.searchValidationErrMsg).toEqual(
-      "태그의 개수는 최대 5개까지 가능합니다."
-    );
-  });
-
-  it("검색 예외가 발생하면 에러 모달이 true가 되고, 닫으면 false가 된다.", () => {
-    const { result } = renderHook(() => useArticleSearch());
-
-    act(() => {
-      const emptyKeyword = "";
-      result.current.handleSearch(emptyKeyword);
-    });
-
-    expect(result.current.isSearchValidationErrorModalOpened).toBe(true);
-
-    act(() => {
-      result.current.handleCloseSearchValidationModal();
-    });
-
-    expect(result.current.isSearchValidationErrorModalOpened).toBe(false);
+    expect(useNavigate()).toHaveBeenCalledWith("/search?keyword=test&tag=tag1");
   });
 });
