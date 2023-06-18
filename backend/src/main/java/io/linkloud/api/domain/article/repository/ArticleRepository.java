160571b1424c0a0a11bf364c8f68750f46032f87
Rope-player <dhqnf11@naver.com>
2023-06-18 15:49:24 +0900

    feat: 제목+글 내용 검색을 위한 JPA 쿼리 작성

--- backend/src/main/java/io/linkloud/api/domain/article/repository/ArticleRepository.java
+++ backend/src/main/java/io/linkloud/api/domain/article/repository/ArticleRepository.java
@@ -23,4 +23,12 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     // 제시된 키워드를 내용(description)으로 가지는 엔티티 조회
     Page<Article> findByDescriptionContainingIgnoreCase(String keyword, Pageable pageable);
 
+    // 제시된 키워드를 제목(title) + 내용(description)으로 가지는 엔티티 조회
+    // 해당 SQL질의는 article 테이블을 한 번만 조회함.
+    /*
+    * SELECT * FROM article
+    * WHERE title LIKE '%{titleKeyword}%' OR description LIKE '%{descriptionKeyword}%'
+    */
+    Page<Article> findByTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String titleKeyword, String descriptionKeyword, Pageable pageable);
+
 }
