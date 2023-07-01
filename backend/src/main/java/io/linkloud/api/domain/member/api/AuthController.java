051c664524f102b7ff97167380c3d27f2713f244
Min <als904204@naver.com>
2023-07-01 12:35:39 +0900

    refactor:리프레시토큰 GET 변경

--- backend/src/main/java/io/linkloud/api/domain/member/api/AuthController.java
+++ backend/src/main/java/io/linkloud/api/domain/member/api/AuthController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +33,7 @@ public class AuthController {
         return ResponseEntity.ok(new SingleDataResponse<>(responseDto));
     }
 
-    @PostMapping("/refresh")
+    @GetMapping("/refresh")
     public ResponseEntity<SingleDataResponse<AuthResponseDto>> refreshAccessToken(
         @CookieValue("refreshToken") String refreshToken,
         HttpServletResponse response) {
