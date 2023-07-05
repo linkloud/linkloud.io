89d004449921674451f813f2a14c857c4dd88cde
a-ryang <chnwu.kim@gmail.com>
2023-07-05 18:08:54 +0900

    refactor: useScrollToElement mount 확인

--- client/src/hooks/useScrollToElement.ts
+++ client/src/hooks/useScrollToElement.ts
@@ -1,4 +1,4 @@
-import { useRef, useEffect, DependencyList } from "react";
+import { useState, useRef, useEffect, DependencyList } from "react";
 
 interface Return<T> {
   ref: React.RefObject<T>;
@@ -8,12 +8,17 @@ function useScrollToElement<T extends HTMLElement>(
   dependencies: DependencyList
 ): Return<T> {
   const ref = useRef<T>(null);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    if (ref.current) {
+    if (isMounted && ref.current) {
       ref.current.scrollIntoView({ behavior: "smooth" });
     }
   }, dependencies);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   return { ref };
 }
