bc53f211d4f7c3dfc542d925aa4e0d33960e8b29
a-ryang <chnwu.kim@gmail.com>
2023-07-05 18:10:45 +0900

    feat: SelectableChip 컴포넌트

---
+++ client/src/components/Chip/SelectableChip.tsx
@@ -0,0 +1,42 @@
+import { MouseEventHandler } from "react";
+import clsx from "clsx";
+import { StrictPropsWithChildren } from "@/types";
+import { ChipProps } from "./Chip";
+
+const styleNames = {
+  primary: "text-white bg-primary-high",
+  neutral: "text-neutral-600 bg-white",
+};
+
+const sizes = {
+  sm: "h-6 px-2",
+  md: "h-9 px-4",
+};
+
+export interface SelectableChipProps extends Omit<ChipProps, "styleName"> {
+  isSelected?: boolean;
+  onClick?: MouseEventHandler<HTMLDivElement>;
+}
+
+export const SelectableChip = ({
+  size = "md",
+  className = "",
+  isSelected = false,
+  onClick,
+  children,
+}: StrictPropsWithChildren<SelectableChipProps>) => {
+  const styleName: keyof typeof styleNames = isSelected ? "primary" : "neutral";
+  return (
+    <span
+      className={clsx(
+        "flex justify-center items-center font-semibold rounded-full link-shadow-sm cursor-pointer transition-colors",
+        styleNames[styleName],
+        sizes[size],
+        className
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </span>
+  );
+};
