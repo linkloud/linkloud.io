a63f7136ab5ad6c59b7c929f683bc059cb6a175b
Min <als904204@naver.com>
2023-07-12 22:28:17 +0900

    feat:게시글 상태 요청,응답 dto 생성

---
+++ backend/src/main/java/io/linkloud/api/domain/article/dto/ArticleStatusRequest.java
@@ -0,0 +1,13 @@
+package io.linkloud.api.domain.article.dto;
+
+
+import io.linkloud.api.domain.article.model.ArticleStatus;
+import lombok.Getter;
+
+
+@Getter
+public class ArticleStatusRequest {
+
+    private ArticleStatus articleStatus;
+
+}
