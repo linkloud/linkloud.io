f56162a27a840cf9bedf5ac265151ef0bfea2263
FaberJoo <sjdk0805@gmail.com>
2023-07-16 17:34:51 +0900

    feat: ArticleRepository 내가 등록한 게시글 조회 태그 및 정렬 적용

--- backend/src/main/java/io/linkloud/api/domain/article/repository/ArticleRepositoryCustom.java
+++ backend/src/main/java/io/linkloud/api/domain/article/repository/ArticleRepositoryCustom.java
@@ -2,6 +2,7 @@ package io.linkloud.api.domain.article.repository;
 
 import io.linkloud.api.domain.article.dto.ArticleResponseDto;
 import io.linkloud.api.domain.article.model.Article;
+import io.linkloud.api.domain.member.model.Member;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,5 +10,6 @@ import org.springframework.data.domain.Pageable;
 public interface ArticleRepositoryCustom {
     // 목록 조회 : 검색
     Page<ArticleResponseDto> findArticleListBySearch(String keyword, List<String> tags, Pageable pageable);
+    Page<ArticleResponseDto> findMyArticleByTag(Member m, String t, String articleStatus, Pageable pageable);
 
 }
