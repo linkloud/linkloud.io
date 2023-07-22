874b2a115ca2af5202ae1a100c1c2cb8f2a95590
Min <als904204@naver.com>
2023-07-22 18:45:00 +0900

    feat:회원 커스텀 레포지토리 구현체 생성 #129

---
+++ backend/src/main/java/io/linkloud/api/domain/member/repository/MemberRepositoryImpl.java
@@ -0,0 +1,10 @@
+package io.linkloud.api.domain.member.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepositoryImpl {
+
+}
