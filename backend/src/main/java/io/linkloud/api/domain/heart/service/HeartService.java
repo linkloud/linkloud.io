14b592499b94a904cf9e2ac28cce8a85f10501d3
Rope-player <dhqnf11@naver.com>
2023-07-28 17:30:42 +0900

    feat: 좋아요를 생성하는 로직 추가

--- backend/src/main/java/io/linkloud/api/domain/heart/service/HeartService.java
+++ backend/src/main/java/io/linkloud/api/domain/heart/service/HeartService.java
@@ -1,10 +1,13 @@
 package io.linkloud.api.domain.heart.service;
 
 import static io.linkloud.api.global.exception.ExceptionCode.LogicExceptionCode.ARTICLE_NOT_FOUND;
+import static io.linkloud.api.global.exception.ExceptionCode.LogicExceptionCode.MEMBER_ALREADY_EXISTS;
 import static io.linkloud.api.global.exception.ExceptionCode.LogicExceptionCode.MEMBER_NOT_FOUND;
 
 import io.linkloud.api.domain.article.model.Article;
 import io.linkloud.api.domain.article.repository.ArticleRepository;
+import io.linkloud.api.domain.heart.dto.HeartRequestDto;
+import io.linkloud.api.domain.heart.dto.HeartResponseDto;
 import io.linkloud.api.domain.heart.model.Heart;
 import io.linkloud.api.domain.heart.repository.HeartRepository;
 import io.linkloud.api.domain.member.model.Member;
@@ -34,11 +37,29 @@ public class HeartService {
             .orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
     }
 
-    /** 좋아요한 아티클ID로 좋아요한 아티클 찾기 */
+    /** 좋아요ID로 좋아요 찾기 */
     @Transactional
     public Heart fetchHeartById(Long id) {
         return heartRepository.findById(id)
             .orElseThrow(() -> new CustomException(ARTICLE_NOT_FOUND));  // 임시로 지정
+    }
+
+    /** 좋아요 생성 */
+    @Transactional
+    public HeartResponseDto addHeart(Long memberId, Long articleId, HeartRequestDto heartRequestDto) {
+        Member member = fetchMemberById(memberId);
+        Article article = fetchArticleById(articleId);
+
+        // 이미 좋아요한 게시글인지 검증
+        if (heartRepository.findByMemberAndArticle(member, article).isPresent()) {
+            //TODO: 409코드 -> 예외 처리 알맞게 변경
+            throw new CustomException(MEMBER_ALREADY_EXISTS);
+        }
+
+        Heart createdHeart = heartRequestDto.toLikeEntity(member, article);
+        heartRepository.save(createdHeart);
+
+        return new HeartResponseDto(createdHeart);
     }
 
 }
