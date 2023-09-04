d10fc305afd5296ed97418f673bb758199429e43
Min <als904204@naver.com>
2023-09-04 19:28:09 +0900

    feat:회원 게시글 상태 변경 entity,repository,service

---
+++ backend/src/main/java/io/linkloud/api/domain/member/repository/MemberArticleStatusRepository.java
@@ -0,0 +1,14 @@
+package io.linkloud.api.domain.member.repository;
+
+import io.linkloud.api.domain.article.model.Article;
+import io.linkloud.api.domain.member.model.Member;
+import io.linkloud.api.domain.member.model.MemberArticleStatus;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberArticleStatusRepository extends JpaRepository<MemberArticleStatus,Long> {
+
+    Optional<MemberArticleStatus> findByMemberAndArticle(Member member, Article article);
+}
