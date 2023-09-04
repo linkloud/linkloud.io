07cf818eaed4427b4edcedf6451f8126e0e9be74
Min <als904204@naver.com>
2023-09-04 19:31:42 +0900

    feat:ReadStatus 응답 Dto

--- backend/src/main/java/io/linkloud/api/domain/article/dto/ArticleResponseDtoV2.java
+++ backend/src/main/java/io/linkloud/api/domain/article/dto/ArticleResponseDtoV2.java
@@ -1,9 +1,30 @@
 package io.linkloud.api.domain.article.dto;
 
 
+import io.linkloud.api.domain.article.model.ReadStatus;
+import io.linkloud.api.domain.member.model.MemberArticleStatus;
+import lombok.Getter;
+
 public class ArticleResponseDtoV2 {
 
     // 게시글 저장 responseDTO
     public record ArticleSave(Long id) {}
+
+    // 특정 회원의 게시글 상태 response
+    @Getter
+    public static class MemberArticleStatusResponse {
+        private final Long statusId;
+        private final ReadStatus readStatus;
+        private final Long memberId;
+        private final Long articleId;
+
+        public MemberArticleStatusResponse(MemberArticleStatus memberArticleStatus) {
+            this.statusId = memberArticleStatus.getId();
+            this.readStatus = memberArticleStatus.getReadStatus();
+            this.memberId = memberArticleStatus.getMember().getId();
+            this.articleId = memberArticleStatus.getArticle().getId();
+        }
+    }
+
 
 }
--- backend/src/main/java/io/linkloud/api/domain/article/model/ReadStatus.java
+++ backend/src/main/java/io/linkloud/api/domain/article/model/ReadStatus.java
@@ -1,11 +1,17 @@
 package io.linkloud.api.domain.article.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 
+@AllArgsConstructor
 @Getter
 public enum ReadStatus {
-    UNREAD,
-    READING,
-    READ;
+    UNREAD("unread"),
+    READING("reading"),
+    READ("read");
+
+    private String status;
+
+
 }
