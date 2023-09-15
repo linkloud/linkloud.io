deae71707e039f6c618d04cefd9cbce33eaa4afa
Min <als904204@naver.com>
2023-09-15 14:53:52 +0900

    feat:중복된 태그 요청 필터링 Service 로직

--- backend/src/main/java/io/linkloud/api/domain/tag/service/TagService.java
+++ backend/src/main/java/io/linkloud/api/domain/tag/service/TagService.java
@@ -1,6 +1,9 @@
 package io.linkloud.api.domain.tag.service;
 
+import io.linkloud.api.domain.article.dto.ArticleRequestDtoV2.ArticleUpdateRequestDto;
+import io.linkloud.api.domain.article.model.Article;
 import io.linkloud.api.domain.tag.dto.TagDto;
+import io.linkloud.api.domain.tag.model.ArticleTag;
 import io.linkloud.api.domain.tag.model.Tag;
 import io.linkloud.api.domain.tag.repository.TagRepository;
 import io.linkloud.api.global.exception.ExceptionCode.LogicExceptionCode;
@@ -8,6 +11,7 @@ import io.linkloud.api.global.exception.CustomException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -15,6 +19,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -34,6 +39,7 @@ public class TagService {
         return tags;
     }
 
+    // TODO : 중복 태그 안걸러지고 그냥 저장됨
     // 중복 Tag 조회 후 저장.
     private Tag addtag(String tagName) {
         // 동일한 태그가 존재하면 바로 반환.
@@ -65,6 +71,68 @@ public class TagService {
 
     public List<TagDto.Response> fetchTagListBySearch(String keyword) {
         return tagRepository.findTagByNameIsStartingWith(keyword);
+    }
+
+
+
+    /**
+     * 중복 태그 제거,존재하지 않는 태그 필터,새로운 태그 DB 저장
+     * 1. 중복으로 요청한 태그가 있다면 중복 태그를 제거한다.
+     * 2. 수정하려는 게시글이 이미 가지고 있는 태그 Name 들을 가져온다
+     * 3. 요청한 태그가 수정하려는 게시글이 이미 가지고 있는 태그 Name 들과 비교해 다른것들만 필터링
+     * 4. 필터링된 태그들을 DB 에 저장
+     */
+    public void tagFilterAndSave(ArticleUpdateRequestDto updateDto, Article article) {
+        // 중복으로 요청온 태그 제거
+        // ex)) tag1,tag1,tag1,tag2 = tag1,tag2
+        List<String> uniqueTags = validateDuplicatedTagRequest(updateDto);
+
+        // 게시글에 이미 연결된 태그 리스트
+        List<String> existingTags = getArticleTagLists(article);
+
+        // 존재하지 않는 태그 필터
+        List<String> filteredTags = notExistsTagsFilter(uniqueTags, existingTags);
+
+        // 새로운 태그 DB 저장
+        List<Tag> savedTags = addTags(filteredTags);
+
+        // 게시글
+        List<ArticleTag> articleTags = addArticleTags(article, savedTags);
+
+        article.addArticleTag(articleTags);
+    }
+
+    // 한번에 똑같은 태그 요청 시 중복 태그 제거
+    private List<String> validateDuplicatedTagRequest(ArticleUpdateRequestDto updateDto) {
+        return updateDto.getTags()
+            .stream()
+            .distinct()
+            .toList();
+    }
+
+    // 수정하려는 게시글의 모든 태그 Name 조회
+    private List<String> getArticleTagLists(Article article) {
+        return article.getArticleTags()
+            .stream()
+            .map(ArticleTag::getTag)
+            .map(Tag::getName)
+            .toList();
+    }
+
+
+    // 존재하지 않는 태그 필터
+    private List<String> notExistsTagsFilter(List<String> targetTags, List<String> existingTags) {
+        return targetTags.stream()
+            .filter(tag -> !existingTags.contains(tag))
+            .collect(Collectors.toList());
+    }
+
+    // Article 객체와 Tag 객체의 리스트를 받아서, 이 둘을 연결하는 ArticleTag 객체의 리스트를 생성
+    private  List<ArticleTag> addArticleTags(Article article, List<Tag> addedTags) {
+        return addedTags
+            .stream()
+            .map(tag -> new ArticleTag(article, tag))
+            .collect(Collectors.toList());
     }
 
     private void verifyExistTag(String tagName) {
