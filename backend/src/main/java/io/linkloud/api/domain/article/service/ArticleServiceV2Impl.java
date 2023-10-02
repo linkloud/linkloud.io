c06ce7a315ea78d6a07656f1a2d181e4c6acea69
Min <als904204@naver.com>
2023-10-02 23:42:55 +0900

    fix:게시글 작성자 여부 버그 수정

--- backend/src/main/java/io/linkloud/api/domain/article/service/ArticleServiceV2Impl.java
+++ backend/src/main/java/io/linkloud/api/domain/article/service/ArticleServiceV2Impl.java
@@ -195,7 +195,7 @@ public class ArticleServiceV2Impl implements ArticleServiceV2{
     private void flagArticlesForLoggedInMember(Long loginMemberId,Long startArticleId,Long endArticleId,
         Slice<ArticleListResponse> articles){
 
-        flagAuthorForArticles(articles, loginMemberId, startArticleId, endArticleId);
+        flagAuthorForArticles(articles, loginMemberId);
         flagReadStatusForArticles(articles, loginMemberId, startArticleId, endArticleId);
         flagLikedStatusForArticles(articles, loginMemberId);
     }
@@ -250,20 +250,20 @@ public class ArticleServiceV2Impl implements ArticleServiceV2{
 
 
     // 게시글 작성자 설정
-    private void flagAuthorForArticles(Slice<ArticleListResponse> articles, Long loginMemberId,
-        Long startArticleId, Long endArticleId) {
+    private void flagAuthorForArticles(Slice<ArticleListResponse> articles, Long loginMemberId) {
 
-        List<Long> myArticleIds = findMyArticleIds(loginMemberId, startArticleId, endArticleId);
-
+        List<Long> myArticleIds = findMyArticleIds(loginMemberId);
+        log.info("{}번 회원이 작성한 총 게시글 수 : {}", loginMemberId, myArticleIds.size());
         for (ArticleListResponse listResponse : articles) {
             if (myArticleIds.contains(listResponse.getId())) {
+                log.info("{}번 회원 {}번 게시글 작성자 설정 완료",loginMemberId,listResponse.getId());
                 listResponse.setAuthor(true);
             }
         }
     }
 
-    private List<Long> findMyArticleIds(Long loginMemberId,Long startId,Long endId) {
-        return articleRepository.findByMemberIdAndIdBetween(loginMemberId,startId,endId)
+    private List<Long> findMyArticleIds(Long loginMemberId) {
+        return articleRepository.findByMemberId(loginMemberId)
             .stream()
             .map(Article::getId)
             .toList();
