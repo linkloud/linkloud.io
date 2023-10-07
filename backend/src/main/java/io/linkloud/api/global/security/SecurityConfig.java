97f48c36bacb8887a4bde0889eeaca8df90f64e1
Min <als904204@naver.com>
2023-10-08 03:15:51 +0900

    refactor:cors settings

--- backend/src/main/java/io/linkloud/api/global/security/SecurityConfig.java
+++ backend/src/main/java/io/linkloud/api/global/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http
-            .cors()
+            .cors().configurationSource(corsConfigurationSource())
             .and()
             .csrf().disable()
             .formLogin().disable()
@@ -60,9 +60,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://linkloud.io"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
-        configuration.setAllowedHeaders(List.of("*"));
+        configuration.addAllowedOrigin("https://linkloud.io");
+        configuration.addAllowedOrigin("http://localhost:5173");
+
+        configuration.addAllowedMethod("*"); // 모든 메소드 허용.
+        configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
