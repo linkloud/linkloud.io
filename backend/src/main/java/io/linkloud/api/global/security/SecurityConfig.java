54f698fa4f6cb565ff2f34ff514d2d58d4db1f11
Min <als904204@naver.com>
2023-10-08 16:17:05 +0900

    cleanup:and() method

--- backend/src/main/java/io/linkloud/api/global/security/SecurityConfig.java
+++ backend/src/main/java/io/linkloud/api/global/security/SecurityConfig.java
@@ -29,7 +29,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http
-            .and()
             .csrf().disable()
             .formLogin().disable()
             .httpBasic().disable()
